7c20ab3e85d8405f9cced087f0466449a0ec5041
Eduard Albu <eduard@eduard.md>
2016-03-12 01:39:34 +0200

    Initial commit

---
+++ app/src/main/java/com/softrangers/sonarcloudmobile/User.java
@@ -0,0 +1,26 @@
+package com.softrangers.sonarcloudmobile;
+
+/**
+ * Created by eduard on 3/12/16.
+ */
+public class User {
+
+    private String mEmail;
+    private String mPassword;
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getmPassword() {
+        return mPassword;
+    }
+
+    public void setmPassword(String mPassword) {
+        this.mPassword = mPassword;
+    }
+}
